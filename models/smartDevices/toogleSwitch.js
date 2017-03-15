7cfd4069d45339a8dfc5e6852da9aa892760a7ad
Tomasz Klęk <tklek@icloud.com>
2017-03-15 13:17:46 +0100

    add toogleSwitch component

---
+++ models/smartDevices/toogleSwitch.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const config = require('../../config/database');
+
+//User schema
+const ToogleSwitchSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: Boolean,
+        required: true
+    },
+});
+
+const ToogleSwitch = module.exports = mongoose.model('ToogleSwitch', ToogleSwitchSchema);
+
+module.exports.changeToogleSwitchState = (id, newState, callback) => {
+    const query1 = {_id: id}
+    const query2 = {$set: {state: newState}};
+    ToogleSwitch.update(query1, query2, callback);
+}
+
+module.exports.getToogleSwitches = (callback) => {
+     ToogleSwitch.find(callback);
+}
+module.exports.deleteToogleSwitch = (toogleSwitch, callback) => {
+     ToogleSwitch.remove(toogleSwitch, callback);
+}
+
+module.exports.addToogleSwitch = (newToogleSwitch, callback) => {
+    newToogleSwitch.save(callback);
+}
---
+++ routes/smartDevices/toogleSwitches.js
@@ -0,0 +1,62 @@
+ const express = require('express');
+ const router = express.Router();
+ const config = require('../../config/database');
+
+ const ToogleSwitch = require('../../models/smartDevices/toogleSwitch'); 
+
+ //add ToogleSwitch route
+ router.post('/add', (req, res, next) => {
+    let newToogleSwitch = new ToogleSwitch({
+         name: req.body.name,
+         description: req.body.description,
+         state: req.body.state
+    });
+
+    ToogleSwitch.addToogleSwitch(newToogleSwitch, (err) => {
+        if(err) {
+            res.json({ success: false, msg: 'Failed to add Toogle Switch'});
+        } else {
+            res.json({ success: true, msg: 'Toogle Switch added'});
+        }
+    });
+ });
+
+ //change State ToogleSwitch route
+ router.post('/changeState', (req, res, next) => {
+
+    let id = req.body.id;
+    let newState = req.body.state;
+    
+    ToogleSwitch.changeToogleSwitchState(id, newState, (err) => {
+        if(err) {
+            res.json({ success: false, msg: 'Failed to change Toogle Switch state'});
+        } else {
+            res.json({ success: true, msg: 'State changed'});
+        }
+    });
+ });
+ //delete ToogleSwitch route
+ router.post('/delete', (req, res, next) => {
+
+    let toogleSwitch = req.body.toogleSwitch;
+
+    ToogleSwitch.deleteToogleSwitch(toogleSwitch, (err) => {
+        if(err) {
+            res.json({ success: false, msg: 'Failed to delete Toogle Switch state'});
+        } else {
+            res.json({ success: true, msg: 'Toogle Switch deleted'});
+        }
+    });
+ });
+
+//get ToogleSwitch route
+ router.get('/', (req, res) => {
+    ToogleSwitch.getToogleSwitches((err, toogleSwitches) => {
+        if(err) {
+            return res.json({ success: false, msg: 'Toogle Switches not found'});
+        }
+        res.json(toogleSwitches);
+    });
+ });
+
+ module.exports = router;
